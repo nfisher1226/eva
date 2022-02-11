d63958d132b8fe975b107a9df2b8890ce0d1ff82
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-02-11 00:12:49 -0500

    Updated doc comments and README

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,14 @@
 //! Contents
 //! ========
 //! * [Introduction](#introduction)
-//! * [Building](#building)
 //! * [Features](#features)
+//! * [Keybindings](#keybindings)
+//! * [Building](#building)
 //! ## Introduction
 //! Eva is a [gemini protocol](https://gemini.circumlunar.space/) browser written in
 //! [Rust](https://rust-lang.org) using the [gtk+](https://gtk-rs.org/) toolkit. Eva
 //! is currently alpha quality software under heavy development and as such is
 //! missing features and may have bugs.
-//!
-//! ## Building
-//! ```sh
-//! # clone the source
-//! git clone https://codeberg.org/jeang3nie/eva.git
-//! cd eva
-//! cargo run -- gemini://gemini.circumlunar.space
-//! ```
 //! ## Features
 //! - [x] tabbed interface
 //! - [ ] bookmarks
@@ -27,6 +20,32 @@
 //!   - [ ] colors
 //! - [x] back-forward list
 //! - [ ] history
+//!
+//! ## Keybindings
+//! | Key | Action |
+//! | --- | --- |
+//! | Ctrl/T | New tab |
+//! | Ctrl/N | New window |
+//! | Ctrl/W | Close tab |
+//! | Ctrl/Q | Close window |
+//! | Ctrl/R | Reload page |
+//! | Alt/Home | Go to homepage |
+//! | Ctrl/PageDown | Next tab |
+//! | Ctrl/PageUp | Previous tab |
+//! | Alt/[1-9] | nth tab |
+//! | Ctrl/Shift/O | Open bookmarks |
+//! | Ctrl/D | Bookmark page |
+//! | Ctrl/H | Open History |
+//! | Ctrl/Shift/P | Open preferences |
+//! | Ctrl/Shift/A | Open about dialog |
+//!
+//! ## Building
+//! ```sh
+//! # clone the source
+//! git clone https://codeberg.org/jeang3nie/eva.git
+//! cd eva
+//! cargo run -- gemini://gemini.circumlunar.space
+//! ```
 use clap::{App, Arg};
 use std::sync::Mutex;
 /// Handles getting the configuration data to and from disk
