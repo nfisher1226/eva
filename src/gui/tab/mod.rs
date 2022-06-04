090f5fd31f984f92bedf75f1df0ee614fc6cfd9a
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-06-03 22:49:46 -0400

    Split tab label into module

--- src/gui/tab/mod.rs
+++ src/gui/tab/mod.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 pub mod bookmark_editor;
-pub use bookmark_editor::BookmarkEditor;
+pub mod label;
+pub use {bookmark_editor::BookmarkEditor, label::Label};
 
 use {
     super::uri,
@@ -11,53 +12,6 @@ use {
     url::Url,
 };
 
-#[derive(Clone, Debug)]
-/// Allows persistent access to the tab label, spinner indicator and close button
-pub struct Label {
-    handle: gtk::Box,
-    label: gtk::Label,
-    spinner: gtk::Spinner,
-    close_button: gtk::Button,
-}
-
-impl Default for Label {
-    fn default() -> Self {
-        let handle = gtk::builders::BoxBuilder::new()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(3)
-            .build();
-        let label = gtk::Label::new(Some("about:blank"));
-        let spinner = gtk::Spinner::new();
-        spinner.set_visible(false);
-        let close_button = gtk::builders::ButtonBuilder::new()
-            .icon_name("window-close-symbolic")
-            .has_frame(false)
-            .build();
-        handle.append(&label);
-        handle.append(&spinner);
-        handle.append(&close_button);
-        Self {
-            handle,
-            label,
-            spinner,
-            close_button,
-        }
-    }
-}
-
-impl Label {
-    pub fn handle(&self) -> gtk::Box {
-        self.handle.clone()
-    }
-
-    pub fn label(&self) -> gtk::Label {
-        self.label.clone()
-    }
-
-    pub fn close_button(&self) -> gtk::Button {
-        self.close_button.clone()
-    }
-}
 
 
 #[derive(Clone, Debug)]
@@ -370,14 +324,7 @@ impl Tab {
     }
 
     pub fn set_label(&self, label: &str, spin: bool) {
-        self.label.label.set_label(label);
-        if spin {
-            self.label.spinner.show();
-            self.label.spinner.start();
-        } else {
-            self.label.spinner.stop();
-            self.label.spinner.hide();
-        }
+        self.label.set(label, spin);
     }
 
     pub fn request_eva_page(&self, uri: &str) {
